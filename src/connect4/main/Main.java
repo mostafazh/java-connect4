28da3785fe3e126624bba3f85011da6d47c66ede
Mina <Mina@localhost>
2010-02-06 23:38:27 +0100

    (no commit message)

---
+++ src/connect4/main/Main.java
@@ -0,0 +1,34 @@
+package connect4.main;
+
+import javax.swing.JOptionPane;
+
+import org.apache.log4j.Logger;
+
+import connect4.engine.ConnectFourEngine;
+import connect4.engine.Player;
+import connect4.ui.CUI;
+import connect4.ui.GUI;
+import connect4.ui.GUI2;
+import connect4.ui.UserInterface;
+
+public class Main {
+	
+	private static Logger logger = Logger.getLogger(Main.class.getName());
+	
+	public static void main(String[] args) {
+		Player p1 = new Player(1);
+		Player p2 = new Player(2);
+		ConnectFourEngine e = ConnectFourEngine.initInstance(p1, p2);
+		logger.info("Game Started");
+		int result = JOptionPane.showConfirmDialog(null, "Enable Graphics ?", "What do you want?", JOptionPane.OK_CANCEL_OPTION);
+		
+		UserInterface ui;
+		if(result == JOptionPane.OK_OPTION)
+			ui = new GUI();
+		else
+			ui = new CUI();
+		ui.setEngine(e);
+		ui.show();
+		ui.start();
+	}
+}
